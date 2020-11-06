a3a933a4f59ff5f523ee3c87f591252a166088ac
RuiHirano <r.hrn.0930@gmail.com>
2020-11-06 17:45:45 +0900

    add user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-import { Moment } from "moment";
+import moment, { Moment } from "moment";
+import { v4 as uuid } from "uuid"
 
 export interface Environment {
     OS: string
@@ -17,4 +18,52 @@ export interface Content {
     Environment: Environment
     Solution: string
     TimeStamp: Moment
+}
+
+
+
+// User
+export class User {
+    ID: string
+    Profile: Profile
+    Setting: Setting
+    constructor() {
+        this.ID = uuid()
+        this.Profile = {
+            Name: "",
+            Birthday: moment(),
+            Message: "",
+            Sex: "MALE",
+            Avatar: "",
+            Header: "",
+        }
+        this.Setting = <Setting>{
+            Email: "",
+            Language: "JAPANESE",
+            Notification: <Notification>{
+                Email: true,
+                Push: true,
+            },
+        }
+    }
+}
+
+export interface Profile {
+    Name: string,
+    Birthday: Moment,
+    Sex: string,
+    Message: string,
+    Avatar: string,
+    Header: string,
+}
+
+export interface Setting {
+    Email: string,
+    Language: string,
+    Notification: Notification,
+}
+
+export interface Notification {
+    Email: boolean,
+    Push: boolean,
 }
